a9e3dc217f651130a89c74ffd101170dd6ab435e
Patrick | Chulsoon <patrick.yang.1248@gmail.com>
2022-04-07 00:40:40 +0900

    SealContext

---
+++ stubs/types.py
@@ -0,0 +1,16 @@
+from typing import Tuple
+
+import numpy as np
+
+std_array = tuple
+std_string = str
+std_vector = list
+std_size_t = int
+std_uint64_t = np.uint64
+
+
+# unique enough?
+# using hash_block_type = std::array<std::uint64_t, hash_block_uint64_count>;
+hash_block_type = Tuple[std_uint64_t, std_uint64_t, std_uint64_t, std_uint64_t]
+# hash_block_type = std_array[std_uint64_t, std_uint64_t, std_uint64_t, std_uint64_t]
+parms_id_type = hash_block_type
