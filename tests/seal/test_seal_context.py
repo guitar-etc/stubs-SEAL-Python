f45abe28f26a6d449ca1c290a6319147014ec31e
Patrick | Chulsoon <patrick.yang.1248@gmail.com>
2022-04-09 15:41:31 +0900

    wip

--- tests/seal/test_seal_context.py
+++ tests/seal/test_seal_context.py
@@ -1,10 +1,14 @@
+import seal
 from seal import CoeffModulus, EncryptionParameters, scheme_type
 
 
 # example_ckks_basics()
 def test_seal_context():
-    parms = EncryptionParameters(scheme=scheme_type.ckks)
+    print(type(seal.scheme_type))
+    parms = EncryptionParameters(scheme_type.ckks)
 
-    poly_modulus_degree = 8192.2
+    poly_modulus_degree = 8192
     parms.set_poly_modulus_degree(poly_modulus_degree)
-    parms.set_coeff_modulus(CoeffModulus.Create(poly_modulus_degree,  60, 40, 40, 60 ))
+    parms.set_coeff_modulus(
+        CoeffModulus.Create(poly_modulus_degree, [60, 40, 40, 60])
+    )
