79244737f8dbd3cb4eeb77c85e10c10dd2a149a5
Patrick | Chulsoon <patrick.yang.1248@gmail.com>
2022-04-09 15:40:56 +0900

    scripts

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo apt-get install git build-essential cmake python3 python3-dev python3-pip python3-venv clang
+sudo apt-get install git build-essential cmake python3.10 python3.10-dev python3-pip python3.10-venv clang
 git submodule update --init --recursive
 
 python3 -m venv .venv
@@ -12,11 +12,12 @@ python3 -m pip install -r requirements.txt
 cd SEAL
 cmake -S . -B build -DSEAL_USE_MSGSL=OFF -DSEAL_USE_ZLIB=OFF -DSEAL_USE_ZSTD=OFF -D CMAKE_CXX_COMPILER="clang" -D CMAKE_C_COMPILER="clang"
 
-cmake --build build
+cmake --build build -j9
 cd ..
 
-python3 setup.py build_ext -i
+python3.10 setup.py build_ext -i
 mv seal.cpython* ../
 cd ..
 
-stubgen -m seal -o stubs
+# This will overwrite?
+# stubgen -m seal -o stubs
